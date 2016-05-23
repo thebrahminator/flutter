05362d202a121ec1f789fa220469c029ee290b1d
ibrahmin13 <vishwa.nath@outlook.com>
2016-05-23 18:03:17 +0530

    Defined the paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'pages/index'
 
-  get 'pages/home'
+  root 'pages#index'
 
-  get 'pages/profile'
+  #get 'pages/index'
 
-  get 'pages/explore'
+  get 'home' => 'pages#home'
+
+  get 'profile' => 'pages#profile'
+
+  get 'explore' => 'pages#explore'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
